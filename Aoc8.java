8b2116d4a826890c72de39437cedcffcd466f62a
artem <unbe@users.noreply.github.com>
2022-12-08 21:52:24 +0100

    day8 in somewhat obfuscated Java

--- Aoc8.java
+++ Aoc8.java
@@ -24,13 +24,13 @@ public class Aoc8 {
 			updateLineVisibility(map, visible, i, i + 1, 1, map[0].length - 1, -1, -1);
 		}
 		for (int j = 0; j < map[0].length; j++) {
-			updateLineVisibility(map, visible, 0, map.length, 1, j, j+1, 1);
-			updateLineVisibility(map, visible, map.length -1, -1, -1, j, j+1, 1);
+			updateLineVisibility(map, visible, 0, map.length, 1, j, j + 1, 1);
+			updateLineVisibility(map, visible, map.length - 1, -1, -1, j, j + 1, 1);
 		}
 		int count = 0;
 		for (int i = 0; i < visible.length; i++) {
 			for (int j = 0; j < visible[i].length; j++) {
-				if(visible[i][j]) {
+				if (visible[i][j]) {
 					count++;
 				}
 			}
@@ -38,8 +38,8 @@ public class Aoc8 {
 		System.out.println(count);
 	}
 
-	private static void updateLineVisibility(int[][] map, boolean[][] visible, int istart, int iend, int istep, int jstart, int jend,
-			int jstep) {
+	private static void updateLineVisibility(int[][] map, boolean[][] visible, int istart, int iend, int istep,
+			int jstart, int jend, int jstep) {
 		int blocker = -1;
 		for (int i = istart; i != iend; i += istep) {
 			for (int j = jstart; j != jend; j += jstep) {
@@ -57,12 +57,12 @@ public class Aoc8 {
 			Arrays.fill(view[i], 1);
 		}
 		for (int i = 0; i < map.length; i++) {
-  		  updateLineView(map, view, i, i+1, 1, 0, map[0].length, 1, 0, 1);
-  		  updateLineView(map, view, i, i + 1, 1, map[0].length - 1, -1, -1, 0, 1);
+			updateLineView(map, view, i, i + 1, 1, 0, map[0].length, 1, 0, 1);
+			updateLineView(map, view, i, i + 1, 1, map[0].length - 1, -1, -1, 0, 1);
 		}
 		for (int j = 0; j < map[0].length; j++) {
-			updateLineView(map, view, 0, map.length, 1, j, j+1, 1, 1, 0);
-			updateLineView(map, view, map.length -1, -1, -1, j, j+1, 1, 1, 0);
+			updateLineView(map, view, 0, map.length, 1, j, j + 1, 1, 1, 0);
+			updateLineView(map, view, map.length - 1, -1, -1, j, j + 1, 1, 1, 0);
 		}
 		int max = 0;
 		for (int i = 0; i < view.length; i++) {
@@ -71,13 +71,13 @@ public class Aoc8 {
 		System.out.println(max);
 	}
 
-	private static void updateLineView(int[][] map, int[][] view,int istart, int iend, int istep, int jstart, int jend,
+	private static void updateLineView(int[][] map, int[][] view, int istart, int iend, int istep, int jstart, int jend,
 			int jstep, int imul, int jmul) {
 		int[] blockers = new int[10];
-		Arrays.fill(blockers, istart*imul + jstart*jmul);
+		Arrays.fill(blockers, istart * imul + jstart * jmul);
 		for (int i = istart; i != iend; i += istep) {
 			for (int j = jstart; j != jend; j += jstep) {
-				int idx = i*imul + j*jmul;
+				int idx = i * imul + j * jmul;
 				int blockerIdx = blockers[map[i][j]];
 				view[i][j] *= Math.abs(idx - blockerIdx);
 				for (int b = 0; b <= map[i][j]; b++) {
