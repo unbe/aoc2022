643149ff1e66a14c9e85660f3b43c13673b743ee
artem <unbe@users.noreply.github.com>
2022-12-10 21:23:40 +0100

    day10 in Rust and I have never written any rust before

--- aocA2.rs
+++ aocA2.rs
@@ -3,38 +3,29 @@ use std::io::{self, prelude::*, BufReader};
 
 fn main() -> io::Result<()> {
     let reader = BufReader::new(File::open("input.txt")?);
-    let mut timeline: Vec<(i32, i32)> = Vec::new();
-    let mut counter = 1;
-    let mut x = 1;
-    timeline.push((counter, x));
-    for line in reader.lines() {
-      let line_s = line.unwrap();
-      let mut split = line_s.split_whitespace();
-      let cmd = split.next().unwrap();
-      let diff :i32;
-      let time;
-      if cmd == "addx" {
-        diff = split.next().unwrap().parse().unwrap(); 
-        time = 2;
-      } else {
-        diff = 0;
-        time = 1;
-      }
-      counter += time;
-      x += diff;
-      if diff != 0 {
-        timeline.push((counter, x))
-      }
-    }
-    let mut tmidx = 0;
+    let mut lines = reader.lines();
+    let mut x : i32 = 1;
+    let mut wait = 1;
+    let mut diff = 0;
     for ray in 1..240 {
-        let col = (ray - 1) % 40 + 1;
-        let c = if (timeline[tmidx].1 - col).abs() <= 1 { '#' } else { '.' };
-        print!("{}", c);
-        if timeline[tmidx].0 < ray {
-            tmidx += 1
+        wait -= 1;
+        if wait == 0 {
+          x += diff;
+          let line = lines.next().unwrap().unwrap();
+          let mut split = line.split_whitespace();
+          let cmd = split.next().unwrap();
+          if cmd == "addx" {
+             diff = split.next().unwrap().parse().unwrap(); 
+             wait = 2;
+           } else {
+              diff = 0;
+              wait = 1;
+           }
         }
-        if col == 40 {
+        let col = (ray - 1) % 40;
+        let c = if (x - col).abs() <= 1 { '#' } else { '.' };
+        print!("{}", c);
+        if col == 39 {
           print!("\n");
         }
     }
